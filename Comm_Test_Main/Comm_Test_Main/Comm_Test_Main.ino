01232c023b75059b86136203e19f0b4a5a63348c
torbenküstermann <kuestermann.to-it21@it.dhbw-ravensburg.de>
2024-05-10 17:35:03 +0200

    add initial Comm_Test_Main to repository

---
+++ Comm_Test_Main/Comm_Test_Main/Comm_Test_Main.ino
@@ -0,0 +1,26 @@
+#include <SPI.h>
+
+const int slaveSelectPin = 10; // Chip Select-Pin des Slave-Arduino
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  pinMode(slaveSelectPin, INPUT);
+  //digitalWrite(slaveSelectPin, HIGH); // Setzen Sie den Chip Select-Pin zu High, um die Kommunikation zu beginnen
+  Serial.println("Initialized, ready to receive:");
+}
+
+void loop() {
+  if (digitalRead(slaveSelectPin) == HIGH) {
+    Serial.println("I'm here ...");
+    delay(1000);
+  }
+  
+  if (digitalRead(slaveSelectPin) == LOW) {
+    int receivedValue = SPI.transfer(69); // Empfangen Sie den Wert vom Master
+    Serial.println("Receiving Stuff ...");
+    Serial.println(receivedValue);
+
+    SPI.transfer(receivedValue); // Senden Sie den Wert zurück an den Master
+  }
+}
